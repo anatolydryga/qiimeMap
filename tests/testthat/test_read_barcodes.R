53615e00ad06623f8fcc51b1b269e77a9038b596
Anatoly Dryga <anatolydryga@gmail.com>
2014-10-29 16:46:16 -0400

    read barcodes from fiel or DB

---
+++ tests/testthat/test_read_barcodes.R
@@ -0,0 +1,10 @@
+context("Read barcode from file")
+
+test_that("read correct number of rows, columns", {
+    expect_true(FALSE)
+} )
+
+
+test_that("no duplicate barcodes", {
+    expect_true(FALSE)
+} )
