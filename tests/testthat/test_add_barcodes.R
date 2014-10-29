80bbbd8043124f91a59dcbfc2d32465d7266d694
Anatoly Dryga <anatolydryga@gmail.com>
2014-10-29 16:45:24 -0400

    add barcodes to well position information

---
+++ tests/testthat/test_add_barcodes.R
@@ -0,0 +1,13 @@
+context("Adding barcodes to well plate, plate number")
+
+test_that("column names as qiime expects", {
+    expect_true(FALSE)
+} )
+
+test_that("correct barcodes are used", {
+    expect_true(FALSE)
+} )
+
+test_that("each well position, plate number is matched to barcodes", {
+    expect_true(FALSE)
+} )
