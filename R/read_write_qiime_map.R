caf0a351d71d8f8e191d0a38e7319246f1ba8f11
Anatoly Dryga <anatolydryga@gmail.com>
2014-11-03 14:49:28 -0500

    read/write qiime input file

---
+++ R/read_write_qiime_map.R
@@ -0,0 +1,9 @@
+read_qiime_map <- function(fileName) {
+    map <- read.table(fileName, sep="\t", as.is=TRUE, header=TRUE)
+    map
+}
+
+write_qiime_map <- function(map, fileName) {
+    write.table(map, fileName, sep="\t")
+    TRUE
+}
--- tests/testthat/test_write_qiime_map.R
+++ tests/testthat/test_write_qiime_map.R
@@ -1,9 +1,15 @@
-context("Writing qiime mapping file")
+context("Writing and reading qiime mapping file")
 
-test_that("column names as qiime expects", {
-    expect_true(FALSE)
-} )
+pos2barcodes <- read_barcodes("PlateNumberWellPosition2Barcodes.txt")
+well_plate_map <- transform_96wellPlate_to_map(read_96_well_plate("well_correct.txt"), 2)
+mapping_file <- add_barcodes(well_plate_map, pos2barcodes)
 
-test_that("tab is used", {
-    expect_true(FALSE)
+test_that("can write a file", {
+    expect_true(write_qiime_map(mapping_file, "mapping_file.txt"))
+})
+
+test_that("write/read are consistent", {
+    write_qiime_map(mapping_file, "mapping_file.txt")
+    map <- read_qiime_map("mapping_file.txt")
+    expect_equivalent(mapping_file, map)
 } )
